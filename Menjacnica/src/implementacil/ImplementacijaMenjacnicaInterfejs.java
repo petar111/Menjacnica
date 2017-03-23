1b6d06a2cc49e1c31945b47ad4afa49f0cd66b1c
petar111 <jpetar765@gmail.com>
2017-03-23 19:39:55 +0100

    Implementirana klasa i dodati atributi
    U klasu ImplementacijaMenjacnicaInterfejs dodati atributi naziv,
    skraceniNaziv i kursevi. Takodje su implementirane sve metode interfejsa
    MenjacnicaInterfejs.

--- Menjacnica/src/implementacil/ImplementacijaMenjacnicaInterfejs.java
+++ Menjacnica/src/implementacil/ImplementacijaMenjacnicaInterfejs.java
@@ -1,28 +1,47 @@
 package implementacil;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejsi.MenjacnicaInterfejs;
 import valuta.kurs.Kurs;
 
 public class ImplementacijaMenjacnicaInterfejs implements MenjacnicaInterfejs {
+	
+	private String naziv;
+	private String skraceniNaziv;
+	private LinkedList<Kurs> kursevi = new LinkedList<Kurs>();
 
 	@Override
 	public void dodavanjeKursaValuteZaDatum(GregorianCalendar datum, double prodajniKurs, double srednjiKurs,
 			double kupovniKurs) {
-		// TODO Auto-generated method stub
-
+		Kurs novi = new Kurs();
+		try {
+			novi.setDatum(datum);
+			novi.setKupovniKurs(kupovniKurs);
+			novi.setProdajniKurs(prodajniKurs);
+			novi.setSrednjiKurs(srednjiKurs);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		kursevi.add(novi);
 	}
 
 	@Override
 	public void brisanjeKursaValuteZaDatum(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for(int i = 0 ; i < kursevi.size() ; i++){
+			if(kursevi.get(i).getDatum().equals(datum))
+				kursevi.remove(i);
+		}
 
 	}
 
 	@Override
 	public Kurs vratiKursZaDatum(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for(int i = 0 ; i < kursevi.size() ; i++){
+			if(kursevi.get(i).getDatum().equals(datum))
+				return kursevi.get(i);
+		}
 		return null;
 	}
 
