0c770a9ac9340a903b7f4a9ef70326a996d98b90
petar111 <jpetar765@gmail.com>
2017-03-23 11:43:52 +0100

    Napravljene izmene u Klasi Valuta. Izbirisani atributi srednjiKurs, prodajniKurs i kupovniKurs kao i njihove get i set metode, dodat atribut kursevi kao i njegove get i set metode, napravljena klasa Kurs sa atributima datum, srednjiKurs, prodajniKurs i kupovniKurs kao i njihovim get i set metodama.

--- Menjacnica/src/valuta/Valuta.java
+++ Menjacnica/src/valuta/Valuta.java
@@ -1,14 +1,22 @@
 package valuta;
 
+import java.util.LinkedList;
+
+import valuta.kurs.Kurs;
+
 public class Valuta {
 	private String naziv;
 	private String skraceniNaziv;
-	private double prodajniKurs;
-	private double kupovniKurs;
-	private double srednjiKurs;
+	private LinkedList<Kurs> kursevi = new LinkedList<Kurs>();
 	
 	
 	
+	public LinkedList<Kurs> getKursevi() {
+		return kursevi;
+	}
+	public void setKursevi(LinkedList<Kurs> kursevi) {
+		this.kursevi = kursevi;
+	}
 	public String getNaziv() {
 		return naziv;
 	}
@@ -21,66 +29,8 @@ public class Valuta {
 	public void setSkraceniNaziv(String skraceniNaziv) {
 		this.skraceniNaziv = skraceniNaziv;
 	}
-	public double getProdajniKurs() {
-		return prodajniKurs;
-	}
-	public void setProdajniKurs(double prodajniKurs) {
-		this.prodajniKurs = prodajniKurs;
-	}
-	public double getKupovniKurs() {
-		return kupovniKurs;
-	}
-	public void setKupovniKurs(double kupovniKurs) {
-		this.kupovniKurs = kupovniKurs;
-	}
-	public double getSrednjiKurs() {
-		return srednjiKurs;
-	}
-	public void setSrednjiKurs(double srednjiKurs) {
-		this.srednjiKurs = srednjiKurs;
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		long temp;
-		temp = Double.doubleToLongBits(kupovniKurs);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
-		temp = Double.doubleToLongBits(prodajniKurs);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
-		temp = Double.doubleToLongBits(srednjiKurs);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Valuta other = (Valuta) obj;
-		if (Double.doubleToLongBits(kupovniKurs) != Double.doubleToLongBits(other.kupovniKurs))
-			return false;
-		if (naziv == null) {
-			if (other.naziv != null)
-				return false;
-		} else if (!naziv.equals(other.naziv))
-			return false;
-		if (Double.doubleToLongBits(prodajniKurs) != Double.doubleToLongBits(other.prodajniKurs))
-			return false;
-		if (skraceniNaziv == null) {
-			if (other.skraceniNaziv != null)
-				return false;
-		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
-			return false;
-		if (Double.doubleToLongBits(srednjiKurs) != Double.doubleToLongBits(other.srednjiKurs))
-			return false;
-		return true;
-	}
+	
+	
 	
 	
 }
